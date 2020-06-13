49dc1406cbd1e7e2233bb63cedebf4c4e0d937a0
Sauhard192 <42416459+Sauhard192@users.noreply.github.com>
2020-06-13 19:54:25 +0545

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,89 @@
+$(document).ready(function() {
+  'use strict';
+
+  var headerOverlay = $(".header__overlay"),
+    menuOpenIcon = $(".nav__icon-menu"),
+    menuCloseIcon = $(".nav__close"),
+    menuList = $(".main-nav__box"),
+    searchOpenIcon = $(".nav__icon-search"),
+    searchCloseIcon = $(".search__close"),
+    searchBox = $(".search");
+
+  /* =======================
+  // Menu and Search
+  ======================= */
+  menuOpenIcon.click(function() {
+    menuOpen();
+  })
+
+  menuCloseIcon.click(function () {
+    menuClose();
+  })
+
+  searchOpenIcon.click(function () {
+    searchOpen();
+  });
+
+  searchCloseIcon.click(function () {
+    searchClose();
+  });
+
+  headerOverlay.click(function () {
+    menuClose();
+    searchClose();
+  });
+
+  function menuOpen() {
+    menuList.addClass("is-open");
+    headerOverlay.addClass("is-visible");
+  }
+
+  function menuClose() {
+    menuList.removeClass("is-open");
+    headerOverlay.removeClass("is-visible");
+  }
+
+  function searchOpen() {
+    searchBox.addClass("is-visible");
+  }
+
+  function searchClose() {
+    searchBox.removeClass("is-visible");
+  }
+
+  /* =======================
+  // Responsive Videos
+  ======================= */
+  $(".post__content, .page__content").fitVids({
+    customSelector: ['iframe[src*="ted.com"]']
+  });
+
+  /* =======================
+  // Zoom Image
+  ======================= */
+  $(".page img, .post img").attr("data-action", "zoom");
+  $(".page a img, .post a img").removeAttr("data-action", "zoom");
+
+  /* =======================
+  // Scroll Top Button
+  ======================= */
+  $(".top").click(function() {
+    $("html, body")
+      .stop()
+      .animate({ scrollTop: 0 }, "slow", "swing");
+  });
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > $(window).height()) {
+      $(".top").addClass("is-active");
+    } else {
+      $(".top").removeClass("is-active");
+    }
+  });
+
+  /* =============================
+  // WOW Animation When You Scroll
+  ============================= */
+  $(".page img, .post img").addClass("wow fadeIn");
+  new WOW().init();
+
+});
